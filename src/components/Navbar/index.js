1ba6ff543d55691a9f390a6aad939f47f871058c
Neil <eric.neil@accenturefederal.com>
2021-10-19 10:51:22 -0500

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Nav, NavLink } from './NavbarElements';
+
+function Navbar() {
+    return (
+        <>
+            <Nav>
+                <NavLink to='/'>Pizza</NavLink>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar
