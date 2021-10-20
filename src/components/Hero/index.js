cc9ce98a1ab15bfbbfe7690786048ae4bc75f9e4
Neil <eric.neil@accenturefederal.com>
2021-10-20 15:54:08 -0500

    Product and Hero Added

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from '../Navbar';
+import Sidebar from '../Sidebar';
 import { HeroContainer, HeroContent, HeroP, HeroH1, HeroBtn, HeroItems } from './HeroElements';
 
 const Hero = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
     return (
        <HeroContainer>
-        <Navbar />
+        <Navbar toggle={toggle}/>
+        <Sidebar isOpen={isOpen} toggle={toggle}/>
         <HeroContent>
             <HeroItems>
             <HeroH1>GREATNESS IN PIZZA</HeroH1>
---
+++ src/components/Products/data.js
@@ -0,0 +1,19 @@
+export const productData = [
+    {
+        img: 'URL(http://www.italytravel.com/wp-content/uploads/2014/04/pizza-margherita.jpg)',
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, and pesto',
+        price: '$19.99',
+        button: 'Add to Cart'
+
+    },
+    {
+     img: 'URL(https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/mbjmqxeh23uxuqca2afg)',
+     alt: 'Pizza', 
+     name: 'Hawaiian Paradise',
+     desc: 'Marinara sauce, basil, italian sauage, roma tomatoes, and pesto',
+     price: '$16.99',
+     button: 'Add to Cart'  
+    }
+]
